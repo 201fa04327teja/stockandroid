2c0eb40aaa1b503d92ad05f21b121a4ccdfb44c4
201fa04327teja <201fa04327teja@gmail.com>
2025-04-24 12:12:38 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
 $host = "sql.freedb.tech";
-$user = "freedb_rental";
-$pass = "tSY&Fb&63D9j9!N";
-$dbname = "freedb_rental";
-$port = 3306; // Specify the port
+$user = "freedb_namratha";
+$pass = '&aUpT8W67qTKUHE';  // ✅ Using single quotes to avoid issues with special characters
+$dbname = "freedb_stockApp";
+$port = 3306;
 
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 } else {
-  //  echo "Database connected successfully!";
+    echo "Database connected successfully!";
 }
 ?>
