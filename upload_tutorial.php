198f66a4cc317d23362dfeab1edb32ddf41b7447
201fa04327teja <201fa04327teja@gmail.com>
2025-04-24 18:29:25 +0530

    Update upload_tutorial.php

--- upload_tutorial.php
+++ upload_tutorial.php
@@ -2,6 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 include 'db.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
