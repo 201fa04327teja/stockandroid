581c8e09e84e1d4ca04e4caa5176b09c8d20d0c1
201fa04327teja <201fa04327teja@gmail.com>
2025-04-29 06:22:10 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -7,38 +7,12 @@
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
-
-
-    CREATE TABLE properties (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        description TEXT NOT NULL,
-        rent DECIMAL(10,2) NOT NULL,
-        address TEXT NOT NULL,
-        city VARCHAR(100) NOT NULL,
-        bedrooms INT NOT NULL,
-        bathrooms INT NOT NULL,
-        area DECIMAL(10,2) NOT NULL,
-        amenities TEXT NOT NULL,
-        image_url VARCHAR(255),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-
-
-
-    CREATE TABLE products (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        description TEXT NOT NULL,
-        rent DECIMAL(10,2) NOT NULL,
-        category VARCHAR(100) NOT NULL,
-        brand VARCHAR(100) NOT NULL,
-        `condition` VARCHAR(50) NOT NULL,
-        availability ENUM('Available', 'Rented') DEFAULT 'Available',
-        image_url VARCHAR(255),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-
-
-
-
+CREATE TABLE blogs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    category VARCHAR(100),
+    image_url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
